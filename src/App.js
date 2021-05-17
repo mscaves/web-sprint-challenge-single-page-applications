d74012af02f67559e9936e97689d38fbcfc06042
Michael Caves <michael.scott.caves@gmail.com>
2021-05-16 21:51:48 -0400

    Done with task 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Switch, Route } from 'react';
+import { useState } from "react";
+import { Switch, Route } from 'react-router-dom';
 
-const formValues = {
+const defaultFormValues = {
   name:'',
   size:'',
   toppings: '',
@@ -9,11 +10,21 @@ const formValues = {
 };
 
 const App = () => {
+  const [formValues, setFormValues] = useState(defaultFormValues);
+  const updateFormValues = (key, value) => {
+    setFormValues({...formValues, [key]:value})
+  };
 
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>Join our membership program to spend even more money and earn extra pounds!</p>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+      <Route path="/pizza">
+        <Form id="pizza-form"></Form>
+      </Route>
     </>
   );
 };
